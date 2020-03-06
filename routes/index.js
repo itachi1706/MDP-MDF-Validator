a35812f64d8f33f9aa1ca8c722adfb3df465ecf5
itachi1706 <kennethsohyq@gmail.com>
2020-03-06 16:12:58 +0800

    Created some placeholder layout style and stuff

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,23 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', increment: function() { return 2; } });
+  res.render('index', { 
+    title: 'MDF Validator (NTU SCSE)', 
+    increment: function() { return 2; },
+    process: function() { alert("Do you know da wae"); }
+  });
+});
+
+function hex2bin(hex){
+  return (parseInt(hex, 16).toString(2)).padStart(8, '0');
+}
+
+router.post('/submitMDF', function (req, res) {
+  console.log("submitMDF()");
+  console.log(req.body.mdf1);
+  console.log(req.body.mdf2);
+  var m1 = hex2bin(req.body.mdf1);
+  res.send("Done");
 });
 
 module.exports = router;
